bb24d0e87660797f01afe731ebdca6bf642c545f
smile54 <s.dudnikovv@gmail.com>
2016-02-27 15:03:38 +0200

    modified:   css.css 	modified:   index.html 	new file:   index.js 	Logo.png

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    var heightR = $(window).height();// высота экрана
+    var widthR = $(window).width();// ширина экрана
+    var logo_Rec = heightR * 0.05;
+    var main_Rec = heightR * 0.9;
+    var msg_Rec = heightR * 0.05;
+
+    $('body .logo').css({'height': logo_Rec});
+    $('body .sidebar').css({'height':heightR});
+    $('body .main').css({'height': main_Rec});
+    $('body .msg').css({'height': msg_Rec});
+});
